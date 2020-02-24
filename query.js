3cc81d9100718f1a4af1cd3486dc7da0cac66d4c
dathoang997 <dathoang997@gmail.com>
2020-02-24 17:00:45 +0700

    log

--- query.js
+++ query.js
@@ -33,6 +33,7 @@ query = async (insertNumber) => {
                   Items.forEach((item) => {
                     result.transactions.forEach(function (e) {
                       let id = e.input.slice(2, 10)
+                      let para = '0x' + e.input.slice(11)
                       if (id !== "0x" && id === item.id) {
                         web3.eth.getTransactionReceipt(e.hash, function (err, receipt) {
                           if (!err && receipt.logs !== undefined && receipt.logs !== null) {
@@ -54,101 +55,54 @@ query = async (insertNumber) => {
                                     if (i === event.inputs.length) {
                                       elog = elog + ')'
                                       elog = elog.replace(', )', ')')
-                                      let myevent = {
-                                        event: elog,
-                                        blockNumber: receipt.blockNumber,
-                                        log_id: receipt.logs[n].id,
-                                      };
-                                      let query = {
-                                        log_id: receipt.logs[n].id
-                                      };
-                                      dbo.collection("save").find(query).toArray(function (err, result) {
-                                        if (err) throw err;
-                                        dbo.collection("save").insertOne(myevent, function (err, res) {
-                                          if (err) throw err;
-                                          console.log("1 document inserted");
-                                          dbo.collection("save").find(query).toArray(function (err, results) {
-                                            if (err) throw err;
-                                            if (results != '') {
-                                              console.log(results);
+                                      var strip_comments = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg
+                                      var argument_names = /([^\s,]+)/g
+                                      var fnStr = item.function.replace(strip_comments, '')
+                                      var parameters = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(argument_names)
+                                      if (parameters === null)
+                                        parameters = []
+                                      let fcut = item.function.indexOf('(')
+                                      let fname = item.function.slice(0, fcut + 1)
+                                      let decode = web3.eth.abi.decodeParameters(parameters, para)
+                                      Accs.forEach((acc) => {
+                                        if (acc.address === e.from || acc.address === e.to) {
+                                          let flog = acc.name + ' ' + fname
+                                          for (let i = 0; i < parameters.length; i++) {
+                                            if (i > 0) {
+                                              flog += parameters[i] + ': ' + decode[i] + ", ";
                                             }
-                                          });
-                                        });
-                                      });
+                                            if (i === parameters.length - 1) {
+                                              flog = flog + ')'
+                                              flog = flog.replace(', )', ')')
+                                              let myfunc = {
+                                                event: elog,
+                                                function: flog,
+                                                blockNumber: receipt.blockNumber,
+                                                log_id: receipt.logs[n].id,
+                                              };
+                                              let query = {
+                                                log_id: receipt.logs[n].id
+                                              };
+                                              dbo.collection("save").find(query).toArray(function (err, result) {
+                                                if (err) throw err;
+                                                // if (result == '')
+                                                dbo.collection("save").insertOne(myfunc, function (err, res) {
+                                                  if (err) throw err;
+                                                  console.log("1 document inserted");
+                                                  dbo.collection("save").find(query).toArray(function (err, results) {
+                                                    if (err) throw err;
+                                                    if (results != '') {
+                                                      console.log(results);
+                                                    }
+                                                  });
+                                                });
+                                              });
+                                            }
+                                          }
+                                        }
+                                      })
                                     }
                                   }
-                                }
-                              }
-                            })
-                          }
-                        })
-                      }
-                      // }
-                    })
-                  })
-                }
-              }
-            })
-
-            web3.eth.getBlock(i, true, function (error, result) {
-              if (!error) {
-                if (result != null && result.transactions != null) {
-                  Items.forEach((item) => {
-                    result.transactions.forEach(function (e) {
-                      let id = e.input.slice(2, 10)
-                      let para = '0x' + e.input.slice(11)
-                      // if (accAddress === e.from || accAddress === e.to) {
-                      if (id !== "0x" && id === item.id) {
-                        web3.eth.getTransactionReceipt(e.hash, function (err, receipt) {
-                          if (!err && receipt.logs !== undefined && receipt.logs !== null) {
-                            Events.forEach((event) => {
-                              for (let n = 0; n <= receipt.logs.length - 1; n++) {
-                                if (event.code === receipt.logs[n].topics[0]) {
-                                  var strip_comments = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg
-                                  var argument_names = /([^\s,]+)/g
-                                  var fnStr = item.function.replace(strip_comments, '')
-                                  var parameters = fnStr.slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')')).match(argument_names)
-                                  if (parameters === null)
-                                    parameters = []
-                                  let fcut = item.function.indexOf('(')
-                                  let fname = item.function.slice(0, fcut + 1)
-                                  let decode = web3.eth.abi.decodeParameters(parameters, para)
-                                  Accs.forEach((acc) => {
-                                    if (acc.address === e.from || acc.address === e.to) {
-                                      let flog = acc.name + ' ' + fname
-                                      for (let i = 0; i < parameters.length; i++) {
-                                        if (i > 0) {
-                                          flog += parameters[i] + ': ' + decode[i] + ", ";
-                                        }
-                                        if (i === parameters.length - 1) {
-                                          flog = flog + ')'
-                                          flog = flog.replace(', )', ')')
-                                          let myfunc = {
-                                            function: flog,
-                                            blockNumber: receipt.blockNumber,
-                                            log_id: receipt.logs[n].id,
-                                          };
-                                          let query = {
-                                            log_id: receipt.logs[n].id
-                                          };
-                                          dbo.collection("save").find(query).toArray(function (err, result) {
-                                            if (err) throw err;
-                                            // if (result == '')
-                                            dbo.collection("save").insertOne(myfunc, function (err, res) {
-                                              if (err) throw err;
-                                              console.log("1 document inserted");
-                                              dbo.collection("save").find(query).toArray(function (err, results) {
-                                                if (err) throw err;
-                                                if (results != '') {
-                                                  console.log(results);
-                                                }
-                                              });
-                                            });
-                                          });
-                                        }
-                                      }
-                                    }
-                                  })
                                 }
                               }
                             })
